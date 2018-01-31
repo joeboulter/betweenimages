d63863a4fae84cc27526d5f6e6605912433aabd0
joeboulter <joe.boulter@gmail.com>
2018-01-31 11:53:56 +0000

    Updated

--- betweenimages.go
+++ betweenimages.go
@@ -1,20 +1,31 @@
 package main
 
 import (
-	"fmt"
+	// "fmt"
 	"image"
 	"image/draw"
 	"image/jpeg"
 	// "math"
 	"github.com/icza/mjpeg"
-	"io/ioutil"
+	// "io/ioutil"
 	"os"
-	"strconv"
+	// "strconv"
+	"bytes"
 )
 
 const magicNumber = 64
 
 func main() {
+
+	checkErr := func(err error) {
+		if err != nil {
+			panic(err)
+		}
+	}
+
+	// Video size: 200x100 pixels, FPS: 24
+	aw, err := mjpeg.New("test.avi", 200, 100, 24)
+	checkErr(err)
 
 	// Get two input images from files and make RGBA images from them
 	existingImageFile, err := os.Open("A.jpg")
@@ -70,36 +81,16 @@ func main() {
 		i += 4
 	}
 
-	makeImages(imgAf, imgBf)
-
-	checkErr := func(err error) {
-		if err != nil {
-			panic(err)
-		}
-	}
-
-	// Video size: 200x100 pixels, FPS: 24
-	aw, err := mjpeg.New("test.avi", 200, 100, 24)
-	
-	checkErr(err)
-
-	// Create a movie from images: 1.jpg, 2.jpg, ..., 10.jpg
-	for j := 0; j < 4; j++ {
-		for i := 1; i < magicNumber + 1; i++ {
-			data, err := ioutil.ReadFile(fmt.Sprintf("%d.jpg", i))
-			checkErr(err)
-			checkErr(aw.AddFrame(data))
-		}
-	}
+	addImages(aw, imgAf, imgBf)
 
 	checkErr(aw.Close())
 
 }
 
-func makeImages(x [300000]float32, y [300000]float32) {
+func addImages(aw mjpeg.AviWriter, x [300000]float32, y [300000]float32) {
 
 	var fn float32
-	for fn = 1; fn < magicNumber + 1; fn++ {
+	for fn = 1; fn < magicNumber+1; fn++ {
 
 		var xPart float32
 		if fn == magicNumber {
@@ -120,55 +111,16 @@ func makeImages(x [300000]float32, y [300000]float32) {
 			j += 4
 		}
 
-		fname := strconv.Itoa(int(fn)) + ".jpg"
-		f, _ := os.Create(fname)
-		defer f.Close()
-		jpeg.Encode(f, img, &jpeg.Options{Quality: 100})
+		checkErr := func(err error) {
+			if err != nil {
+				panic(err)
+			}
+		}
+
+		buf := &bytes.Buffer{}
+		checkErr(jpeg.Encode(buf, img, nil))
+		checkErr(aw.AddFrame(buf.Bytes()))
 
 	}
 
 }
-
-// func recursiveMakeImage(x [300000]float32, y [300000]float32, fn int, g int) {
-// 	fmt.Println("Called recursiveMakeImage with fn =", fn, "and g =", g)
-// 	makeImage(x, y, fn)
-// 	g++
-// 	// This won't work if the guard is an odd number...
-// 	if g < 6 {
-// 		fnBack := fn - int((16 / math.Pow(2, float64(g-1))))
-// 		fnFwd := fn + int((16 / math.Pow(2, float64(g-1))))
-// 		recursiveMakeImage(x, average(x, y), fnBack, g)
-// 		recursiveMakeImage(average(x, y), y, fnFwd, g)
-// 	}
-// }
-
-// func makeImage(x [300000]float32, y [300000]float32, fn int) {
-
-// 	fmt.Println("Called makeImage with fn=", fn)
-
-// 	// Make an image from the average of the two arrays and write it to a file
-// 	img := image.NewRGBA(image.Rect(0, 0, 250, 300))
-// 	j := 0
-// 	for j < (img.Stride * img.Rect.Max.Y) {
-// 		img.Pix[j] = uint8(x[j]/2 + y[j]/2)
-// 		img.Pix[j+1] = uint8(x[j+1]/2 + y[j+1]/2)
-// 		img.Pix[j+2] = uint8(x[j+2]/2 + y[j+2]/2)
-// 		img.Pix[j+3] = uint8(x[j+3]/2 + y[j+3]/2)
-// 		j += 4
-// 	}
-
-// 	fname := strconv.Itoa(fn) + ".jpg"
-// 	f, _ := os.Create(fname)
-// 	defer f.Close()
-// 	jpeg.Encode(f, img, &jpeg.Options{Quality: 100})
-// }
-
-// func average(x [300000]float32, y [300000]float32) [300000]float32 {
-// 	var z [300000]float32
-// 	i := 0
-// 	for i < 300000 {
-// 		z[i] = x[i]/2 + y[i]/2
-// 		i++
-// 	}
-// 	return z
-// }
