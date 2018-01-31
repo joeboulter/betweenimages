3011a86f5a22c33f9f2968ebbee258833137c1ae
joeboulter <joe.boulter@gmail.com>
2018-01-31 16:32:25 +0000

    Update

--- betweenimages.go
+++ betweenimages.go
@@ -5,12 +5,13 @@ import (
 	"image"
 	"image/draw"
 	"image/jpeg"
+	_ "image/png"
 	// "math"
 	"github.com/icza/mjpeg"
 	// "io/ioutil"
-	"os"
-	// "strconv"
 	"bytes"
+	"os"
+	"strconv"
 )
 
 const magicNumber = 64
@@ -23,12 +24,51 @@ func main() {
 		}
 	}
 
-	// Video size: 200x100 pixels, FPS: 24
-	aw, err := mjpeg.New("test.avi", 200, 100, 24)
+	// Video size: 300x300 pixels, FPS: 24
+	aw, err := mjpeg.New("test.avi", 300, 300, 48)
 	checkErr(err)
 
-	// Get two input images from files and make RGBA images from them
-	existingImageFile, err := os.Open("A.jpg")
+	for n := 1; n < 16; n++ {
+		imgA, imgB := getImagesFromFiles(n)
+
+		// Move into floating point. Create two arrays of floats that represent the images.
+		i := 0
+		var imgAf [360000]float32
+		for i < imgA.Stride*imgA.Rect.Max.Y {
+			imgAf[i] = float32(imgA.Pix[i])
+			imgAf[i+1] = float32(imgA.Pix[i+1])
+			imgAf[i+2] = float32(imgA.Pix[i+2])
+			imgAf[i+3] = float32(imgA.Pix[i+3])
+			i += 4
+		}
+
+		var imgBf [360000]float32
+		i = 0
+		for i < imgB.Stride*imgB.Rect.Max.Y {
+			imgBf[i] = float32(imgB.Pix[i])
+			imgBf[i+1] = float32(imgB.Pix[i+1])
+			imgBf[i+2] = float32(imgB.Pix[i+2])
+			imgBf[i+3] = float32(imgB.Pix[i+3])
+			i += 4
+		}
+
+		addImages(aw, imgAf, imgBf)
+	}
+
+	checkErr(aw.Close())
+
+}
+
+func getImagesFromFiles(n int) (*image.RGBA, *image.RGBA) {
+
+	var stA string
+	if n < 10 {
+		stA = "inputs/0"
+	} else {
+		stA = "inputs/"
+	}
+	fn := stA + strconv.Itoa(n) + ".png"
+	existingImageFile, err := os.Open(fn)
 	if err != nil {
 
 	}
@@ -39,11 +79,18 @@ func main() {
 
 	}
 
-	imgA := image.NewRGBA(image.Rect(0, 0, 250, 300))
+	imgA := image.NewRGBA(image.Rect(0, 0, 300, 300))
 	boundsA := inputImgA.Bounds()
 	draw.Draw(imgA, imgA.Rect, inputImgA, boundsA.Min, draw.Over)
 
-	existingImageFile, err = os.Open("B.jpg")
+	var stB string
+	if n < 9 {
+		stB = "inputs/0"
+	} else {
+		stB = "inputs/"
+	}
+	fn = stB + strconv.Itoa(n+1) + ".png"
+	existingImageFile, err = os.Open(fn)
 	if err != nil {
 
 	}
@@ -54,40 +101,14 @@ func main() {
 
 	}
 
-	imgB := image.NewRGBA(image.Rect(0, 0, 250, 300))
+	imgB := image.NewRGBA(image.Rect(0, 0, 300, 300))
 	boundsB := inputImgB.Bounds()
 	draw.Draw(imgB, imgB.Rect, inputImgB, boundsB.Min, draw.Over)
 
-	// At this point, we have imgA and imgB, which are our starting points for creating the interpolated images.
-
-	// Move into floating point. Create two arrays of floats that represent the images.
-	i := 0
-	var imgAf [300000]float32
-	for i < imgA.Stride*imgA.Rect.Max.Y {
-		imgAf[i] = float32(imgA.Pix[i])
-		imgAf[i+1] = float32(imgA.Pix[i+1])
-		imgAf[i+2] = float32(imgA.Pix[i+2])
-		imgAf[i+3] = float32(imgA.Pix[i+3])
-		i += 4
-	}
-
-	var imgBf [300000]float32
-	i = 0
-	for i < imgB.Stride*imgB.Rect.Max.Y {
-		imgBf[i] = float32(imgB.Pix[i])
-		imgBf[i+1] = float32(imgB.Pix[i+1])
-		imgBf[i+2] = float32(imgB.Pix[i+2])
-		imgBf[i+3] = float32(imgB.Pix[i+3])
-		i += 4
-	}
-
-	addImages(aw, imgAf, imgBf)
-
-	checkErr(aw.Close())
-
+	return imgA, imgB
 }
 
-func addImages(aw mjpeg.AviWriter, x [300000]float32, y [300000]float32) {
+func addImages(aw mjpeg.AviWriter, x [360000]float32, y [360000]float32) {
 
 	var fn float32
 	for fn = 1; fn < magicNumber+1; fn++ {
@@ -101,7 +122,7 @@ func addImages(aw mjpeg.AviWriter, x [300000]float32, y [300000]float32) {
 		yPart := 1 - xPart
 
 		// Make an image from the two arrays and write it to a file
-		img := image.NewRGBA(image.Rect(0, 0, 250, 300))
+		img := image.NewRGBA(image.Rect(0, 0, 300, 300))
 		j := 0
 		for j < (img.Stride * img.Rect.Max.Y) {
 			img.Pix[j] = uint8(x[j]*xPart + y[j]*yPart)
